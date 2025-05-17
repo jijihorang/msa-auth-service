dfcf389b838b6d041c04731ec48783751b0e08ff
jijihorang <jiho7213@gmail.com>
2025-05-17 21:02:23 +0900

    배포 확인용

---
+++ src/main/java/com/practice/auth_service/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.practice.auth_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong from auth-service!";
+    }
+}
