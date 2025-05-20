fc7048ee2ccdb44f6005b960293f6c9825514bba
jijihorang <jiho7213@gmail.com>
2025-05-20 14:46:36 +0900

    build test

--- src/main/java/com/practice/auth_service/controller/HealthCheckController.java
+++ src/main/java/com/practice/auth_service/controller/HealthCheckController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
+
     @GetMapping("/ping")
     public String ping() {
         return "git actions + discord 연동 후 알림이 가는지 확인해보자 !";
